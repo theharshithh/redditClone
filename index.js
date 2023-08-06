5a07b79c970d94ba6c8a0848736347327ba8903e
theharshithh <theharshithdev@gmail.com>
2023-08-07 00:30:51 +0530

    the app is ready

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path'); 
+const redditData = require('./data.json'); 
+const hostName = `0.0.0.0`; 
+const port = 3000; 
+
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));  
+
+app.set('view engine', 'ejs'); // Set up EJS view engine
+
+app.get('/', (req, res) => {
+    res.render("home.ejs"); 
+}); 
+
+app.get('/r/:subreddit', (req,res) => {
+    const {subreddit} = req.params;
+    let subredditData = redditData[subreddit]; 
+    if (subredditData) {
+    res.render('subreddit', { ...subredditData });
+    }
+    else  {
+        res.render('404page.ejs', {subreddit}); 
+    }
+     
+})
+
+app.listen(port,hostName, () => {
+    console.log(`Server is running at http:// `+hostName+':'+port+'/');
+});
